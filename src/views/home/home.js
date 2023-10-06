0b7ef69962f33abb4c04e1b8d81d77fad4319fe0
김 다빈 <besesa95@gmail.com>
2023-10-06 23:05:56 +0900

    🚧 Progress: home update
    1. home update

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -16,3 +16,44 @@ window.onload = function () {
 
   setInterval(nextSlide, 3000);
 };
+
+// 신상품
+
+// 신상품 슬라이드
+
+// 베스트
+async function loadProducts() {
+  try {
+    const response = await fetch("../data/bestItem.json");
+    const products = await response.json();
+
+    const container = document.querySelector(".productContainer");
+
+    products.map((product) => {
+      const productDiv = document.createElement("div");
+      productDiv.className = "productItem";
+
+      const img = document.createElement("img");
+      img.className = "productImg";
+      img.src = product.imgSrc;
+
+      const productName = document.createElement("p");
+      productName.className = "productName";
+      productName.textContent = product.name;
+
+      const productPrice = document.createElement("b");
+      productPrice.className = "productPrice";
+      productPrice.textContent = product.price;
+
+      productDiv.appendChild(img);
+      productDiv.appendChild(productName);
+      productDiv.appendChild(productPrice);
+
+      container.appendChild(productDiv);
+    });
+  } catch (error) {
+    console.error("Error loading products:", error.message);
+  }
+}
+
+loadProducts();
