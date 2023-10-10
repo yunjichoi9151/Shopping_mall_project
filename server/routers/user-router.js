5532e8cb10e2837f37955399f72fef874410578b
euni00 <euneee2000@naver.com>
2023-10-10 21:24:49 +0900

    🎯Fix: Fix CRUD code to connect MongoDB

--- server/routers/user-router.js
+++ server/routers/user-router.js
@@ -1,5 +1,9 @@
-// 회원가입하기
 // CRUD 구현하기
+// C : 사용자 정보 생성
+// R : 사용자 정보 조회
+// U : 사용자 정보 수정
+// D : 사용자 정보 삭제
+
 const { Router } = require("express");
 
 const UserModel = require("../db/models/user-model");
@@ -7,45 +11,44 @@ const UserModel = require("../db/models/user-model");
 const router = Router();
 
 // READ 구현하기 -> GET
-router.get("/user", async (req, res) => {
-    console.log(Data);
-    const data = await UserModel.find({});
+router.get("/", async (req, res) => {
+    const user = await UserModel.find({});
 
-    res.json(data);
+    res.json(user);
     
-    // console.log("data OK");
+    console.log("data OK");
 });
 
-router.get("/user/:userId", async (req, res) => {
+router.get("/:userId", async (req, res) => {
     const search_id = req.params.userId;
-    const data = await UserModel.find({ _id: search_id });
+    const user = await UserModel.find({ _id: search_id });
 
-    res.json(data);
+    res.json(user);
 });
 
 
 // CREATE 구현하기 -> POST
-router.post("/user", async (req, res) => {
+router.post("/", async (req, res) => {
     const { _id, name, email, password, joinTime } = req.body;
     
-    const data = await Data.create({
+    const user = await UserModel.create({
         _id,
         name,
         email,
         password,
         joinTime,
     });
-    res.json(data);
+    res.json(user);
 })
 
 // UPDATE 구현하기 -> PUT
-router.put("/user/:data_id", async (req, res) => {
-    const { _id } = req.params;
+router.put("/:userId", async (req, res) => {
+    const { userId } = req.params;
     const { name, email, password, joinTime } = req.body;
 
-    const data = await Data.updateOne(
+    const user = await UserModel.updateOne(
         {
-            _id,
+            _id: userId,
         },
         {
             name,
@@ -54,15 +57,20 @@ router.put("/user/:data_id", async (req, res) => {
             joinTime
         }
     );
-    res.json(data);
+    res.json(user);
+    console.log("Update OK");
 });
 
 // DELETE 구현하기 -> DELETE
-router.delete("/user/:/data_id", async (req, res) => {
-    const { _id } = req.params;
+router.delete("/:userId", async (req, res) => {
+    const { userId } = req.params;
 
-    const data = await Data.deleteOne({ _id });
-    res.send(data);
+    const user = await UserModel.deleteOne({ _id: userId });
+    res.json(user);
+    console.log("Delete OK");
 });
+
+// 로그인 구현
+
 
 module.exports = router;
