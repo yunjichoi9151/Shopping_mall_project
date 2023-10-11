b59955138c066edace1bc70b990d80d3b695bc75
euni00 <euneee2000@naver.com>
2023-10-11 15:26:59 +0900

    ✨ Feat: Add login,logout,join

--- server/routers/user-router.js
+++ server/routers/user-router.js
@@ -5,8 +5,9 @@
 // D : 사용자 정보 삭제
 
 const { Router } = require("express");
-
 const UserModel = require("../db/models/user-model");
+const asyncHandler = require('../middlewares/async-handler');
+const hashPassword = require('../middlewares/hash-password');
 
 const router = Router();
 
@@ -71,6 +72,36 @@ router.delete("/:userId", async (req, res) => {
 });
 
 // 로그인 구현
+
+// 로그인 페이지에서 로그인 되었을 경우, 되지 않았을 경우 어느 경로로 갈지 결정
+router.get('/', (req, res) => {
+    if (req.user) {
+        res.redirect('/user');
+        return;
+    }
+  res.redirect("/login");
+});
+
+// 회원가입 (hashedPassword 사용)
+router.post(
+  '/join',
+  asyncHandler(async (req, res) => {
+    const { email, name, password } = req.body;
+    const hashedPassword = hashPassword(password);
+    const user = await UserModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    res.redirect('/');
+  }),
+);
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
 
 
 module.exports = router;
