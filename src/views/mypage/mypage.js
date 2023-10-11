80f5f28fc26ecce9dfc32be6705f15c2438c703c
김 다빈 <besesa95@gmail.com>
2023-10-11 16:55:58 +0900

    🎯 Fix: css 수정
    1. css 수정

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -19,8 +19,6 @@ async function fetchUserData(userId) {
 // 웹 페이지의 내용을 업데이트하는 함수
 function updateUserInfo(user) {
   const memberSpan = document.querySelector(".username");
-  const cumulativeTotalSpan = document.querySelector(".userCumulativeTotal");
-  const pointSpan = document.querySelector(".userPoint");
 
   const emailInput = document.getElementById("email"); // 이메일 입력 요소 가져오기
   const nameInput = document.getElementById("name"); // 이름 입력 요소 가져오기
@@ -28,8 +26,6 @@ function updateUserInfo(user) {
   const addressInput = document.getElementById("address"); // 주소 입력 요소 가져오기
 
   memberSpan.textContent = `${user.name} 님 안녕하세요`;
-  cumulativeTotalSpan.textContent = `누적 구매금액 : ${user.cumulativeTotal}`;
-  pointSpan.textContent = user.point;
 
   // 각 입력 요소의 value 속성을 설정하여 사용자 정보를 업데이트
   emailInput.value = user.email;
