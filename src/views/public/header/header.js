c9962a58e7c8d5f68ea357ef21595f32279d45a7
yunjichoi9151 <cyjcode99@gmail.com>
2023-10-13 20:49:50 +0900

    ✨ Feat: View Router 수정
    1. 링크 수정

--- src/views/public/header/header.js
+++ src/views/public/header/header.js
@@ -1,58 +1,58 @@
 // 헬스보충제 클릭 시 - 탄수화물 보충제 이동
 function categoryCarbohydrateMove(event) {
   event.preventDefault();
-  window.location.href = "../category/carbohydrate.html";
+  window.location.href = '/carbohydrate';
 }
 // 단백질 클릭 시 - 단백질 보충제 이동
 function categoryProteinMove(event) {
   event.preventDefault();
-  window.location.href = "../category/protein.html";
+  window.location.href = '/protein';
 }
 // 아미노산 클릭 시 - 아미노산 보충제 이동
 function categoryAminoMove(event) {
   event.preventDefault();
-  window.location.href = "../category/amino.html";
+  window.location.href = '/amino';
 }
 // 영양제 클릭 시 - 영양제 이동
 function categoryNutrientsMove(event) {
   event.preventDefault();
-  window.location.href = "../category/nutrients.html";
+  window.location.href = '/nutrients';
 }
 // 헬스용품 클릭 시 - 헬스용품 이동
 function categoryFitnessMove(event) {
   event.preventDefault();
-  window.location.href = "../category/fitness.html";
+  window.location.href = '/fitness';
 }
 // 다이어트 클릭 시 - 다이어트 이동
 function categoryDietMove(event) {
   event.preventDefault();
-  window.location.href = "../category/diet.html";
+  window.location.href = '/diet';
 }
 
 // 로고 클릭 시 - home 으로 이동
 function homeMove(event) {
   event.preventDefault();
-  window.location.href = "../home/home.html";
+  window.location.href = '/home';
 }
 // 사람 클릭 시 - login 으로 이동
 function mypageMove(event) {
   event.preventDefault();
-  window.location.href = "../login/login.html";
+  window.location.href = '/login';
 }
 // 장바구니 클릭 시 - cart 으로 이동
 function cartMove(event) {
   event.preventDefault();
-  window.location.href = "../cart/cart.html";
+  window.location.href = '/cart';
 }
 
 //검색
 function executeSearch() {
-  const input = document.getElementById("searchInput");
+  const input = document.getElementById('searchInput');
   const searchTerm = input.value.trim(); // 공백 제거
 
   if (searchTerm) {
     console.log(`Searching for: ${searchTerm}`);
   } else {
-    alert("검색어를 입력해주세요.");
+    alert('검색어를 입력해주세요.');
   }
 }
