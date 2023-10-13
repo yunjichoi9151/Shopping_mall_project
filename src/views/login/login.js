db80146c27dc94dacccfd39636c4b72abe865cdd
yunjichoi9151 <cyjcode99@gmail.com>
2023-10-14 03:20:13 +0900

    🎯 Fix: html 경로 수정
    1. html로 설정되어 있던 경로들 수정

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,73 +1,73 @@
-const loginInputEmail = document.querySelector("#emailInput");
-const loginInputPassword = document.querySelector("#passwordInput");
+const loginInputEmail = document.querySelector('#emailInput');
+const loginInputPassword = document.querySelector('#passwordInput');
 
 // 회원가입
-const doJoinBtn = document.querySelector("#doJoin");
-doJoinBtn.addEventListener("click", goToJoin);
+const doJoinBtn = document.querySelector('#doJoin');
+doJoinBtn.addEventListener('click', goToJoin);
 
 // 회원가입 페이지로 이동
 function goToJoin() {
   // 경로 수정 필요 !!!!
-  window.location.href = "../join/join.html";
+  window.location.href = '/join';
 }
 
 // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 // 아이디 찾기 버튼 클릭 시 모달 창 열기
-const findIdBtn = document.querySelector("#findId");
-findIdBtn.addEventListener("click", function () {
-  const findIdModal = document.querySelector("#findIdModal");
-  findIdModal.style.display = "block";
+const findIdBtn = document.querySelector('#findId');
+findIdBtn.addEventListener('click', function () {
+  const findIdModal = document.querySelector('#findIdModal');
+  findIdModal.style.display = 'block';
 });
 
 // 비밀번호 찾기 버튼 클릭 시 모달 창 열기
-const findPasswordBtn = document.querySelector("#findPassword");
-findPasswordBtn.addEventListener("click", function () {
-  const findPasswordModal = document.querySelector("#findPasswordModal");
-  findPasswordModal.style.display = "block";
+const findPasswordBtn = document.querySelector('#findPassword');
+findPasswordBtn.addEventListener('click', function () {
+  const findPasswordModal = document.querySelector('#findPasswordModal');
+  findPasswordModal.style.display = 'block';
 });
 
 // 모달 창의 닫기 버튼에 이벤트 리스너 추가
-const closeBtns = document.querySelectorAll(".close");
+const closeBtns = document.querySelectorAll('.close');
 closeBtns.forEach(function (closeBtn) {
-  closeBtn.addEventListener("click", function () {
-    const modals = document.querySelectorAll(".modal");
+  closeBtn.addEventListener('click', function () {
+    const modals = document.querySelectorAll('.modal');
     modals.forEach(function (modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     });
   });
 });
 
 // 사용자가 모달 외부를 클릭하면 모달 창이 닫히도록 설정
-window.addEventListener("click", function (event) {
-  const modals = document.querySelectorAll(".modal");
+window.addEventListener('click', function (event) {
+  const modals = document.querySelectorAll('.modal');
   modals.forEach(function (modal) {
     if (event.target === modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     }
   });
 });
 
 // 아이디 찾기 폼 제출 시 동작
-const findIdForm = document.querySelector("#findIdForm");
-const findIdResult = document.querySelector("#findIdResult");
-findIdForm.addEventListener("submit", handleFind);
+const findIdForm = document.querySelector('#findIdForm');
+const findIdResult = document.querySelector('#findIdResult');
+findIdForm.addEventListener('submit', handleFind);
 async function handleFind(e) {
   e.preventDefault();
   // 아이디 찾기 기능
-  const findNameInput = document.querySelector("#findName");
+  const findNameInput = document.querySelector('#findName');
   const findName = findNameInput.value;
-  const findPhoneNumberInput = document.querySelector("#findPhoneNumber");
+  const findPhoneNumberInput = document.querySelector('#findPhoneNumber');
   const findPhoneNumber = findPhoneNumberInput.value;
 
   const findData = { phoneNumber: findPhoneNumber };
   try {
-    const res = await axios.post("/api/user/find", findData);
+    const res = await axios.post('/api/user/find', findData);
     if (res.data && res.data.email) {
       const userEmail = res.data.email;
       findIdResult.innerText = `아이디 : ${userEmail}`;
     } else {
-      findIdResult.innerText = "사용자를 찾을 수 없습니다.";
+      findIdResult.innerText = '사용자를 찾을 수 없습니다.';
     }
   } catch (err) {
     console.error(err);
@@ -75,19 +75,19 @@ async function handleFind(e) {
 }
 
 // 비밀번호 찾기 폼 제출 시 동작 (임의의 동작, 실제 동작으로 대체)
-const findPasswordForm = document.querySelector("#findPasswordForm");
-const findPasswordResult = document.querySelector("#findPasswordResult");
-findPasswordForm.addEventListener("submit", function (event) {
+const findPasswordForm = document.querySelector('#findPasswordForm');
+const findPasswordResult = document.querySelector('#findPasswordResult');
+findPasswordForm.addEventListener('submit', function (event) {
   event.preventDefault();
   // 비밀번호 찾기 기능 추가해야함 !!!!!
-  findPasswordResult.innerText = "임시 비밀번호가 전송되었습니다.";
+  findPasswordResult.innerText = '임시 비밀번호가 전송되었습니다.';
 });
 
 // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 // 로그인
-const loginBtn = document.querySelector("#loginBtn");
-loginBtn.addEventListener("click", handleLogin);
+const loginBtn = document.querySelector('#loginBtn');
+loginBtn.addEventListener('click', handleLogin);
 
 // 로그인 구현
 async function handleLogin(e) {
@@ -100,7 +100,7 @@ async function handleLogin(e) {
   try {
     const loginUserData = { email, password };
 
-    const res = await axios.post("/api/auth/login", loginUserData);
+    const res = await axios.post('/api/auth/login', loginUserData);
 
     console.log(res);
     console.log(res.data);
@@ -115,18 +115,18 @@ async function handleLogin(e) {
     console.log(refreshToken);
 
     // 로그인 성공, 토큰을 세션 스토리지에 저장
-    localStorage.setItem("token", token);
-    localStorage.setItem("loggedIn", "true");
-    localStorage.setItem("refeshToken", refreshToken);
-    localStorage.setItem("userEmail", userEmail);
+    localStorage.setItem('token', token);
+    localStorage.setItem('loggedIn', 'true');
+    localStorage.setItem('refeshToken', refreshToken);
+    localStorage.setItem('userEmail', userEmail);
 
     // 기본 페이지로 이동
-    window.location.href = "/";
+    window.location.href = '/';
     alert(`${userName} 님, 로그인되었습니다.`);
 
     // 로그인 실패시 alert 창 띄움
   } catch (err) {
     console.error(err);
-    alert("로그인이 실패하였습니다. 아이디와 비밀번호를 확인해주세요.");
+    alert('로그인이 실패하였습니다. 아이디와 비밀번호를 확인해주세요.');
   }
 }
--- src/views/order/orderComplete.js
+++ src/views/order/orderComplete.js
@@ -1,27 +1,27 @@
 // localStorage에서 주문 정보를 가져옴
-const orderInfoJSON = localStorage.getItem("orderInfo");
+const orderInfoJSON = localStorage.getItem('orderInfo');
 const orderInfo = JSON.parse(orderInfoJSON);
 
 // 주문 정보를 화면에 표시
 if (orderInfo) {
-  document.getElementById("order-total-price").textContent =
-    orderInfo.totalPrice + "원";
-  document.getElementById("order-date").textContent = orderInfo.orderTime;
-  document.getElementById("order-name").textContent = orderInfo.recipient;
-  document.getElementById("order-contact").textContent = orderInfo.contact;
-  document.getElementById("order-address").textContent = orderInfo.address;
-  document.getElementById("order-detail-address").textContent =
+  document.getElementById('order-total-price').textContent =
+    orderInfo.totalPrice + '원';
+  document.getElementById('order-date').textContent = orderInfo.orderTime;
+  document.getElementById('order-name').textContent = orderInfo.recipient;
+  document.getElementById('order-contact').textContent = orderInfo.contact;
+  document.getElementById('order-address').textContent = orderInfo.address;
+  document.getElementById('order-detail-address').textContent =
     orderInfo.detailAddress;
 } else {
   // orderInfo 가 제대로 없는 경우
-  alert("주문 정보를 가져올 수 없습니다.");
+  alert('주문 정보를 가져올 수 없습니다.');
   // !!!! 다시 메인 페이지로 이동하거나 장바구니로 이동하도록 구현 필요 !!!!
 }
 
 // 홈으로 가기
-const homeButton = document.getElementById("homeBtn");
-homeButton.addEventListener("click", () => {
-  window.location.href = "../home/home.html";
+const homeButton = document.getElementById('homeBtn');
+homeButton.addEventListener('click', () => {
+  window.location.href = '/';
 });
 
 // 주문정보를 통해 사용자 email 찾음
